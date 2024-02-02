a4e84c26e080f46b035695696dd8fc269e579456
yashlan <yashlan007@gmail.com>
2024-02-03 00:30:12 +0700

    Initial Commit

---
+++ PlantSnap/PlantSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantSnapApp.swift
+//  PlantSnap
+//
+//  Created by M Yashlan on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PlantSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
