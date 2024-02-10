f6cc26f55d530a677bbde4e8c0b40acb48287f17
yashlan <yashlan007@gmail.com>
2024-02-10 18:31:30 +0700

    Create PlantDetails.swift

---
+++ PlantSnap/Model/PlantDetails.swift
@@ -0,0 +1,16 @@
+//
+//  PlantDetails.swift
+//  PlantSnap
+//
+//  Created by M Yashlan on 10/02/24.
+//
+
+
+struct PlantDetails {
+    let hd_image_url: String
+    let common_name: String
+    let scientific_name: String
+    let cycle: String
+    let watering: String
+    let sunlight: [String]
+}
